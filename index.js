bdb68bf257bb0b6f4a1421beba31162507706c8a
Charles Caleb <charlesofficial9@gmail.com>
2024-08-27 15:23:39 +0100

    major update

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".nav-link");
+  const currentUrl = window.location.href;
+
+  navLinks.forEach((link) => {
+    if (link.href === currentUrl) {
+      link.classList.add("active");
+    }
+  });
+});
